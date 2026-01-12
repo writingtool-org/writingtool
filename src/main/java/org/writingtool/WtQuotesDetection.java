afe70ff04315f926d0148524667f6ff85c9d1e45
Fred Kruse <f.kruse@freenet.de>
2026-01-12 17:47:14 +0100

    change AI quotes to original

--- src/main/java/org/writingtool/WtQuotesDetection.java
+++ src/main/java/org/writingtool/WtQuotesDetection.java
@@ -1,3 +1,21 @@
+/* WritingTool, a LibreOffice Extension based on LanguageTool
+ * Copyright (C) 2024 Fred Kruse (https://writingtool.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package org.writingtool;
 
 import java.util.ArrayList;
@@ -7,18 +25,41 @@ import java.util.regex.Pattern;
 
 import org.writingtool.tools.WtMessageHandler;
 
+import com.sun.star.beans.Property;
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.i18n.LocaleDataItem;
+import com.sun.star.i18n.XLocaleData;
+import com.sun.star.lang.Locale;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XComponentContext;
+
 public class WtQuotesDetection {
   
-  private static final List<String> startSymbols = Arrays.asList("“", "„", "»", "«", "\"");
-  private static final List<String> endSymbols   = Arrays.asList("”", "“", "«", "»", "\"");
+  public static List<String> startSymbols = new ArrayList<>(Arrays.asList("“", "„", "»", "«", "\""));
+  public static List<String> endSymbols   = new ArrayList<>(Arrays.asList("”", "“", "«", "»", "\""));
+//  private static final List<String> singleStartSymbols = Arrays.asList("‘", "‚", "›", "‹", "'");
+//  private static final List<String> singleEndSymbols   = Arrays.asList("’", "‘", "‹", "›", "'");
   private static final Pattern PUNCTUATION = Pattern.compile("[\\p{Punct}…–—&&[^\"'_]]");
   private static final Pattern PUNCT_MARKS = Pattern.compile("[\\?\\.!,]");
+  
+  private int nQuote = -1;
+  private int nChanges = 0;
   
   private List<Integer> openingQuotes;
   private List<Integer> closingQuotes;
   
-  private String openQuote = new String();
+  public WtQuotesDetection() {}
 
+  public WtQuotesDetection(XComponentContext xContext, Locale locale) {
+    try {
+      setConfiguredQuotes(xContext, locale);
+    } catch (Throwable e) {
+      WtMessageHandler.showError(e);
+    }
+  }
+  
   /**
    * Defines exceptions from quotes
    */
@@ -44,14 +85,15 @@ public class WtQuotesDetection {
   }
   
   /**
-   * defines an opening quote
+   * returns the index of the opening quote
+   * -1 else
    */
-  private boolean isOpeningQuote(String txt, int i) throws Throwable {
+  private int isOpeningQuote(String txt, int i) throws Throwable {
     String tChar = txt.substring(i, i + 1);
     for (int j = 0; j < startSymbols.size(); j++) {
       if (startSymbols.get(j).equals(tChar)) {
         if (isNotQuote (txt, i, j)) {
-          return false;
+          return -1;
         }
         if (endSymbols.contains(startSymbols.get(j))) {
           return (i == 0
@@ -59,8 +101,42 @@ public class WtQuotesDetection {
               || (i < txt.length() - 1 && !Character.isWhitespace(txt.charAt(i + 1))
                   && ((!PUNCT_MARKS.matcher(txt.substring(i + 1, i + 2)).matches()
                       && PUNCTUATION.matcher(txt.substring(i - 1, i)).matches())
-                      || (txt.charAt(i + 1) == '-'))));
+                      || (txt.charAt(i + 1) == '-')))) ? j : -1;
         }
+        return j;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * returns the index of the closing quote
+   * -1 else
+   */
+  private int isClosingQuote(String txt, int i) throws Throwable {
+    String tChar = txt.substring(i, i + 1);
+    for (int j = 0; j < endSymbols.size(); j++) {
+      if (endSymbols.get(j).equals(tChar)) {
+        if (isNotQuote(txt, i, j) || nQuote != j) {
+          return -1;
+        }
+        return j;
+      }
+    }
+    return -1;
+  }
+  
+  /**
+   * True, if the quotation mark is a potential inch mark.
+   */
+  boolean isPotentialInchMark(String txt, int i) throws Throwable {
+    if(txt.substring(i, i + 1).equals("\"")) {
+      return false;
+    }
+    int j;
+    for (j = i - 1; j >= 0 && !Character.isDigit(txt.charAt(j)); j--);
+    if(j < 0 || Character.isWhitespace(txt.charAt(j))) {
+      if(i + 1 < txt.length() && !txt.substring(i + 1, i + 2).equals(",")) {
         return true;
       }
     }
@@ -68,19 +144,20 @@ public class WtQuotesDetection {
   }
 
   /**
-   * defines an closing quote
+   * returns the index of the closing quote
+   * -1 else
    */
-  private boolean isClosingQuote(String txt, int i) throws Throwable {
+  private int isPotentialClosingQuote(String txt, int i) throws Throwable {
     String tChar = txt.substring(i, i + 1);
     for (int j = 0; j < endSymbols.size(); j++) {
       if (endSymbols.get(j).equals(tChar)) {
-        if (isNotQuote (txt, i, j) && !openQuote.equals(startSymbols.get(j))) {
-          return false;
+        if (isNotQuote(txt, i, j) || isPotentialInchMark(txt, i)) {
+          return -1;
         }
-        return true;
+        return j;
       }
     }
-    return false;
+    return -1;
   }
   
   /**
@@ -93,18 +170,22 @@ public class WtQuotesDetection {
       openingQuotes.add(-1);
     }
     for (int i = 0; i < txt.length(); i++) {
-      if (isOpeningQuote(txt, i)) {
+      int tQuote = isOpeningQuote(txt, i);
+      if (tQuote >= 0) {
         if (i == 0 && openingQuotes.size() > 0) {
           openingQuotes.set(0, i);
         } else {
           openingQuotes.add(i);
         }
-        openQuote = new String(txt.substring(i, i + 1));
+        nQuote = tQuote;
         isQuoteBefore = true;
-      } else if (isClosingQuote(txt, i)) {
-        closingQuotes.add(i);
-        openQuote = new String();
-        isQuoteBefore = false;
+      } else {
+        tQuote = isClosingQuote(txt, i);
+        if (tQuote >= 0) {
+          closingQuotes.add(i);
+          nQuote = -1;
+          isQuoteBefore = false;
+        }
       }
     }
     return isQuoteBefore;
@@ -178,6 +259,185 @@ public class WtQuotesDetection {
     oQuotes.set(nPara, openingQuotes);
     cQuotes.set(nPara, closingQuotes);
     updateTextParagraphs(oQuotes, cQuotes, nPara, isQuoteBefore);
+  }
+  
+  /**
+   * Function to detect wrong double quotes
+   * @throws Throwable 
+   */
+  public boolean isNotCorrectQuote(String txt) throws Throwable {
+    boolean isNotCorrectDouble = false;
+    for (int i = 0; i < txt.length(); i++) {
+      int tQuote = isOpeningQuote(txt, i);
+      if (tQuote >= 0) {
+        if ("\"".equals(startSymbols.get(tQuote))) {
+          isNotCorrectDouble = true;
+        } else if (nQuote < 0) {
+          nQuote = tQuote;
+        } else if (nQuote != tQuote) {
+          isNotCorrectDouble = true;
+        }
+        if (isNotCorrectDouble && nQuote >= 0) {
+          return isNotCorrectDouble;
+        }
+      } else {
+        tQuote = isPotentialClosingQuote(txt, i);
+        if (tQuote >= 0) {
+          if ("\"".equals(endSymbols.get(tQuote))) {
+            isNotCorrectDouble = true;
+          } else if (nQuote != tQuote) {
+            isNotCorrectDouble = true;
+          }
+          if (isNotCorrectDouble && nQuote >= 0) {
+            return isNotCorrectDouble;
+          }
+        }
+      }
+    }
+    return isNotCorrectDouble;
+  }
+
+  /**
+   * Function change wrong double quotes to correct quotes
+   * gives back the corrected String
+   * @throws Throwable 
+   */
+  public String changeToCorrectQuote(String txt) throws Throwable {
+    return changeToCorrectQuote(txt, 0);
+  }
+  
+  private String changeToCorrectQuote(String txt, int iCorrectQuote) throws Throwable {
+    nChanges = 0;
+    if (iCorrectQuote < 0 || iCorrectQuote >= startSymbols.size()) {
+      return txt;
+    }
+    String out = new String(txt);
+    for (int i = 0; i < txt.length(); i++) {
+      int tQuote = isOpeningQuote(txt, i);
+      if (tQuote >= 0) {
+        if (tQuote != iCorrectQuote) {
+          out = out.substring(0, i) + startSymbols.get(iCorrectQuote);
+          if (i < txt.length() - 1) {
+            out += txt.substring(i + 1);
+          }
+          nChanges++;
+        }
+      } else {
+        tQuote = isPotentialClosingQuote(txt, i);
+        if (tQuote >= 0) {
+          if (tQuote != iCorrectQuote) {
+            out = out.substring(0, i) + endSymbols.get(iCorrectQuote);
+            if (i < txt.length() - 1) {
+              out += txt.substring(i + 1);
+            }
+            nChanges++;
+          }
+        }
+      }
+    }
+    return out;
+  }
+
+  /**
+   * get the first possible correct Quote
+   * @throws Throwable 
+   */
+  public int getFirstQuote(String txt) throws Throwable {
+    for (int i = 0; i < txt.length(); i++) {
+      int tQuote = isOpeningQuote(txt, i);
+      if (tQuote >= 0) {
+        if (tQuote < startSymbols.size() - 1) {
+          return tQuote;
+        }
+      } else {
+        tQuote = isPotentialClosingQuote(txt, i);
+        if (tQuote >= 0) {
+          if (tQuote < startSymbols.size() - 1) {
+            return tQuote;
+          }
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * get the detected possible correct Quote
+   */
+  public int getDetectedQuote() {
+    return nQuote;
+  }
+
+  /**
+   * get the number of changes
+   */
+  public int getNumChanges() {
+    return nChanges;
+  }
+
+  private void setConfiguredQuotes(XComponentContext xContext, Locale locale) throws Throwable {
+
+    XMultiServiceFactory xMSF = UnoRuntime.queryInterface(XMultiServiceFactory.class, xContext.getServiceManager());
+    if (xMSF == null) {
+      return;
+    }
+    Object oConfigProvider = xMSF.createInstance("com.sun.star.configuration.ConfigurationProvider");
+    if (oConfigProvider == null) {
+      WtMessageHandler.printToLogFile("oConfigProvider == null");
+      return;
+    }
+    XMultiServiceFactory confMsf = UnoRuntime.queryInterface(XMultiServiceFactory.class, oConfigProvider);
+        
+    PropertyValue[] args = new PropertyValue[1];
+    args[0] = new com.sun.star.beans.PropertyValue();
+    args[0].Name = "nodepath";
+    args[0].Value = "/org.openoffice.Office.Common/AutoCorrect";
+    
+    Object access = confMsf.createInstanceWithArguments(
+        "com.sun.star.configuration.ConfigurationAccess", args);
+
+    XPropertySet props = UnoRuntime.queryInterface(XPropertySet.class, access);
+    
+    int iDoubleStart = (int) props.getPropertyValue("DoubleQuoteAtStart");
+    int iDoubleEnd   = (int) props.getPropertyValue("DoubleQuoteAtEnd");
+//    int iSingleStart = (int) props.getPropertyValue("SingleQuoteAtStart");
+//    int iSingleEnd   = (int) props.getPropertyValue("SingleQuoteAtEnd");
+    
+    String doubleStart = "";
+    String doubleEnd = "";
+
+    if (iDoubleStart == 0 || iDoubleEnd == 0) {
+      Object olocaleData = xMSF.createInstance("com.sun.star.i18n.LocaleData");
+      if (olocaleData == null) {
+        WtMessageHandler.printToLogFile("olocaleData == null");
+        return;
+      }
+      XLocaleData xlocaleData = UnoRuntime.queryInterface(XLocaleData.class, olocaleData);
+      LocaleDataItem localeDataItem = xlocaleData.getLocaleItem(locale);
+      doubleStart = localeDataItem.doubleQuotationStart;
+      doubleEnd = localeDataItem.doubleQuotationEnd;
+//      singleStart = localeDataItem.quotationStart.charAt(0);
+//      singleEnd = localeDataItem.quotationEnd.charAt(0);
+    }
+    if (iDoubleStart != 0) {
+      doubleStart = "" + ((char) iDoubleStart);
+    }
+    if (iDoubleEnd != 0) {
+      doubleEnd = "" + ((char) iDoubleEnd);
+    }
+//    WtMessageHandler.printToLogFile("WtQuotesDetection: readQuotes: doubleStart: " + doubleStart + ", doubleEnd: " + doubleEnd);
+//        + ", singleStart: " + singleStart + ", singleEnd: " + singleEnd);
+    for (int i = startSymbols.size() - 1; i >= 0; i--) {
+      if (startSymbols.get(i).equals(doubleStart)) {
+        if (endSymbols.get(i).equals(doubleEnd)) {
+          startSymbols.remove(i);
+          endSymbols.remove(i);
+        }
+        break;
+      }
+    }
+    startSymbols.add(0, doubleStart);
+    endSymbols.add(0, doubleEnd);
   }
 
 }
